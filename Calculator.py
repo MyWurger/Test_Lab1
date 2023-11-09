93f4f420ed4ce9a3800c039fc2d4b0445f536b78
MyWurger <wdgwdg9@gmail.com>
2023-11-09 22:09:49 +0300

    Change1

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+print("0 в качестве знака операции"
+      "\nзавершит работу программы\n")
+ 
+while True:
+    s = input("Знак (+, -): ")
+    if s == '0':
+        break
+    if s in ('+', '-'):
+        a = float(input("a = "))
+        b = float(input("b = "))
+        if s == '+':
+            print("%.2f" % (a + b))
+        elif s == '-':
+            print("%.2f" % (a - b))
+    else:
+        print("Неверный знак операции!")
