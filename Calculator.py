413f7932a64dcbf6ff9e1a740d6c56698a92a3ad
MyWurger <wdgwdg9@gmail.com>
2023-11-09 22:15:09 +0300

    Change3

--- Calculator.py
+++ Calculator.py
@@ -2,15 +2,22 @@ print("0 в качестве знака операции"
       "\nзавершит работу программы\n")
  
 while True:
-    s = input("Знак (+, -): ")
+    s = input("Знак (+, -, *, /): ")
     if s == '0':
         break
-    if s in ('+', '-'):
+    if s in ('+', '-', '*', '/'):
         a = float(input("a = "))
         b = float(input("b = "))
         if s == '+':
             print("%.2f" % (a + b))
         elif s == '-':
             print("%.2f" % (a - b))
+        elif s == '*':
+            print("%.2f" % (a * b))
+        elif s == '/':
+            if b != 0:
+                print("%.2f" % (a / b))
+            else:
+                print("Деление на ноль!")
     else:
         print("Неверный знак операции!")
